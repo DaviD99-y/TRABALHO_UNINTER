57d3519be8b7e0957601e6e9066ad54e354def50
DaviD99-y <dajddn170@gmail.com>
2025-05-03 12:47:30 -0300

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,7 @@
+let paragrafo = document.querySelector("p2");
+
+paragrafo.addEventListener("click", trocatexto);
+
+function trocatexto(){
+    paragrafo.innerHTML='Se expressarmos gratidão pelo que temos, teremos mais pelo que expressar gratidão.';
+}
